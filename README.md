8bc6049127855df29dd9b445df64d1751c50433d
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-02-16 18:32:31 +0100

    Add license

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 InnoGames Monitoring Plugins
-----------------------------
+============================
 
 This repository is only for Nagios compatible scripts developed by
 InnoGames.  We have packages for more complicated checks.  This
 repository is only for standalone simple scripts which don't worth
 having separate packages.  Please don't bloat it.
+
+License
+-------
+
+The scripts are released under the MIT License.  The license is
+included on the file headers.  The MIT License is registered with
+and approved by the Open Source Initiative [1].
+
+[1] https://opensource.org/licenses/MIT
--- formatnsca.sh
+++ formatnsca.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 #
-# An helper script to format output fro NSCA
+# InnoGames Monitoring Plugins - formatnsca.sh
 #
-# We have some Nagios checks that run long enough not to be used as an active
-# one.  With this wrapper, we will get the data from them to be send to Nagios
-# via NSCA.
+# Copyright (c) 2016, InnoGames GmbH
 #
-# Copyright (c) 2015, InnoGames GmbH
+# This is an helper script to format output for NSCA.  It is useful for checks
+# that run long enough not to be used as an active one.  With this wrapper,
+# you can get the data from them to be send to the monitoring server via NSCA.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 
 if ! [ "$2" ]; then
