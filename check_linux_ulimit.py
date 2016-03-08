1b12c94c2033b2458cfac065cff341aa02827b42
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-03-08 11:27:44 +0100

    Rewrite helper functions of check_linux_ulimit.py

--- check_linux_ulimit.py
+++ check_linux_ulimit.py
@@ -61,17 +61,10 @@ def get_state(warning):
     msg = ''
 
     # compare softlimits with openfiles for all pids
-    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+    pids = [pid for pid in list_proc_db() if pid.isdigit()]
     for pid in pids:
-        piddir = '/proc/' + pid
-        try:
-            num_fds = len(os.listdir(piddir + '/fd'))
-            with open(piddir + '/limits', 'r') as limits_file:
-                limits = limits_file.readlines()
-        except (OSError, IOError):
-            continue
-
-        soft_limit, hard_limit = _get_fdlimits(limits)
+        num_fds = len(list_proc_db(pid, 'fd'))
+        soft_limit = get_proc_ulimit(pid, 'Max open files')
 
         # soft_limit 0 means actually not set (during fork etc)
         if soft_limit > num_fds or soft_limit == 0:
@@ -80,36 +73,66 @@ def get_state(warning):
 
         elif soft_limit <= num_fds:
             state = 2
-            procname = _get_procname(piddir)
             msg += 'PID {0} [{1}] reached its soft limit (open: {2}, limit {3})\n'.format(
-                pid, procname, num_fds, soft_limit
+                pid, get_proc_name(pid), num_fds, soft_limit
             )
 
         elif (soft_limit * warning / 100) <= num_fds:
             if state != 2:
                 state = 1
-            procname = _get_procname(piddir)
             msg += 'PID {0} [{1}] nearly reached its soft limit at {2} open fds\n'.format(
-                pid, procname, num_fds
+                pid, get_proc_name(pid), num_fds
             )
 
     return state, msg
 
-def _get_fdlimits(limits):
-    # parse output of /proc/PID/limits to get max open files
-    a, a, a, soft, hard, a = [
-        line.split() for line in limits if line.startswith('Max open files')
-    ][0]
+def list_proc_db(*dirs):
+    """Return the contents of a directory under the proc file system
 
-    return int(soft), int(hard)
+    We have to handle the exceptions in here, because the proc files
+    change after we read the list.
+    """
 
-def _get_procname(piddir):
-    # get procname and set to unknown if cmdline is empty or not available
+    path = '/proc/' + '/'.join(dirs)
+
     try:
-        with open(piddir + '/cmdline', 'r') as cmdline_file:
-            return cmdline_file.readline().split('\x00')[0]
-    except OSError:
-        return 'unknown'
+        return os.listdir(path)
+    except (OSError, IOError):
+        return []
+
+def read_proc_db(*dirs):
+    """Return the contents of a file under the proc file system
+
+    We have to handle the exceptions in here, because the proc files
+    change after we read the list.
+    """
+
+    path = '/proc/' + '/'.join(dirs)
+
+    try:
+        with open(path, 'r') as proc_file:
+            return proc_file.readlines()
+    except (OSError, IOError):
+        return None
+
+def get_proc_name(pid):
+    """Get the name of the process from the proc file system"""
+
+    cmdline = read_proc_db(pid, 'cmdline')
+
+    return cmdline.split('\x00')[0] if cmdline else 'unknown'
+
+def get_proc_ulimit(pid, name):
+    """Return the soft limit value of the given limit"""
+
+    limits = read_proc_db(pid, 'limits')
+
+    if limits:
+        for line in limits:
+            if line.startswith(name):
+                return line.split()[3]
+
+    return 0
 
 if __name__ == '__main__':
     main()
