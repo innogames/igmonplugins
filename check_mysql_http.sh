77b3c64dca794aeccb3df489b2b073ad0caa4ac9
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-03-31 15:30:36 +0200

    Added new check: check_mysql_http.sh

---
+++ check_mysql_http.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#
+# This script checks if a mysql server is healthy running on 127.0.0.1. It will
+# return:
+# "HTTP/1.x 200 OK\r" (if mysql is running smoothly)
+# - OR -
+# "HTTP/1.x 500 Internal Server Error\r" (else)
+#
+# The purpose of this script is make haproxy capable of monitoring mysql properly
+#
+
+MYSQL_HOST="127.0.0.1"
+MYSQL_PORT="3306"
+MYSQL_OPTS="-N -q -A --connect-timeout=10"
+MYSQL_BIN="/usr/bin/mysql"
+
+CHECK_QUERY="show global status where variable_name='wsrep_local_state'"
+CHECK_QUERY2="show global variables where variable_name='wsrep_sst_method'"
+
+return_ok()
+{
+    echo "HTTP/1.1 200 OK\r\n"
+    echo "Content-Type: text/html\r\n"
+    echo "Content-Length: 43\r\n"
+    echo "\r\n"
+    echo "<html><body>MySQL is running.</body></html>\r\n"
+    echo "\r\n"
+    exit 0
+}
+
+return_fail()
+{
+    echo "HTTP/1.1 503 Service Unavailable\r\n"
+    echo "Content-Type: text/html\r\n"
+    echo "Content-Length: 42\r\n"
+    echo "\r\n"
+    echo "<html><body>MySQL is *down*.</body></html>\r\n"
+    echo "\r\n"
+    exit 1
+}
+
+status=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY;" 2>/dev/null | awk '{print $2;}')
+if [ $? -ne 0 ]; then
+        return_fail
+fi
+if [ $status -eq 2 ] || [ $status -eq 4 ] ; then
+   if [ $status -eq 2 ]; then
+     method=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY2;" 2>/dev/null | awk '{print $2;}')
+     if [ $? -ne 0 ]; then
+        return_fail
+     fi
+     if [ -z "$method" ] || [ "$method" = "rsync" ] || [ "$method" = "mysqldump" ]; then
+         return_fail
+     fi
+   fi
+   return_ok
+fi
+
+return_fail
