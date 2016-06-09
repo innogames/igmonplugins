c25ee2f7d5a05b43eb2c55eb4f5caa207a88fcca
Timo Schiller <timo.schiller@innogames.com>
2016-06-09 16:19:22 +0200

    systemd service check rework

--- check_systemd_service_status.py
+++ check_systemd_service_status.py
@@ -11,69 +11,85 @@
 
 import os
 import re
+import sys
 
 from argparse import ArgumentParser
 
 
+command = 'systemctl --all --no-legend --no-pager list-units'
+default_critical_services = [nginx]
+
+
+class Problem: 
+    # From more important to less
+    failed = 0
+    dead = 1
+    not_loaded_but_not_inactive = 2
+    not_loaded_but_not_dead = 3
+
+    
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('-s', '--status_to_check', default='dead', help="which status should be checked")
-    parser.add_argument('-w', '--warning_limit', default=1, type=int, help="limit for warning status")
-    parser.add_argument('-c', '--critical_limit', default=1, type=int, help="limit for critical status")
-    parser.add_argument('-l', '--services_to_check', default=[], nargs='*', help="list of services to check")
+    parser.add_argument(
+        '--critical-services',
+        nargs='*',
+        help='list of critical services to check',
+    )
 
     return vars(parser.parse_args())
 
+    
+def main(critical_services):
+    if not critical_services:
+        critical_services = default_critical_services
 
-def main(status_to_check, warning_limit, critical_limit, services_to_check):
+    results = []
+    for line in os.popen(command).readlines():
+        service_split = line.strip().split(None, 4)
 
-    number_servs_found = 0
-    list_servs_found = ''
-
-    # get all systemd services
-    services = os.popen("systemctl --all --no-legend --no-pager list-units").read()
-    service_list = services.splitlines()
-
-    # iterate through all services and find the ones which match the status to check
-    for service in service_list:
-        # replace multiple whitespaces with single space
-        service = re.sub( '\s+', ' ', service ).strip()
-        serv_name, serv_load, serv_active, serv_sub, serv_desc = service.split(' ', 4)
-
-        # do we have a list of services we should check?
-        if services_to_check:
-            # is the current service in the list we should check
-            if serv_name.split(".")[0] in services_to_check:
-                # is the the current service in the status we are checking
-                if serv_sub == status_to_check:
-                    number_servs_found += 1
-                    list_servs_found += ' ' + serv_name
-        # else we check all services
-        else:
-            # is the the current service in the status we are checking
-            if serv_sub == status_to_check:
-                number_servs_found += 1
-                list_servs_found += ' ' + serv_name
-
-    # if we have found more services than the critical limit return a critical statuscode
-    # and print the number and list of services
-    if number_servs_found >= critical_limit:
-        print("CRITICAL - " + str(number_servs_found) + " services found with status " + status_to_check + " (" + list_servs_found + " )")
-        return 2
-
-    # if we have found more services than the warning limit return a warning statuscode
-    # and print the number and list of services
-    if number_servs_found >= warning_limit:
-        print("WARNING - " + str(number_servs_found) + " services found with status " + status_to_check + " (" + list_servs_found + " )")
-        return 1
-
-    # only if the warning limit is smaller than the critical limit
-    # we will use it for the status message
-    if warning_limit < critical_limit:
-        print("OK - not more or equal than " + str(warning_limit) + " services found with status " + status_to_check)
+        result = check_service(*service_split[1:4])
+        if result:
+            results.append((result, service_split[0]))
+     
+    if any(r == Problem.failed and s in critical_services for r, s in results):
+        print('CRITICAL: ' + get_message(results))
+        sys.exit(2)
+    
+    if results:
+        print('WARNING: ' + get_message(results))
+        sys.exit(1)
+    
+    print('OK')
+    sys.exit(0)
+    
+    
+def check_service(serv_load, serv_active, serv_sub):
+    if serv_load == 'loaded':
+        if serv_active == 'failed' or serv_sub == 'failed':
+            return Problem.failed
+        
+        if serv_sub == 'dead':
+            return Problem.dead
     else:
-        print("OK - not more or equal than " + str(critical_limit) + " services found with status " + status_to_check)
-    return 0
-
+        if serv_active != 'inactive':
+            return Problem.not_loaded_but_not_inactive
+            
+        if serv_sub != 'dead':
+            return Problem.not_loaded_but_not_dead
+                 
+ 
+def get_message(results):
+    problem_names = {v, k for k, v in vars(Problem) if isinstance(v, int)}
+    message = ''
+    last_problem = None
+    for problem, service in results.sorted():
+        if problem != last_problem:
+            message += problem_names[problem].replace('_', ' ') + ': '
+            last_problem = problem
+        message += service + ' '
+    
+    return message
+  
+  
 if __name__ == '__main__':
     main(**parse_args())
