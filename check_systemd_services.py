f49d5ea9dbdd51d3dab653bc0bc7351ccc2660b0
Emre Hasegeli <emre.hasegeli@innogames.com>
2016-06-10 12:25:21 +0200

    Improve check_systemd_services.py

--- check_systemd_services.py
+++ check_systemd_services.py
@@ -2,94 +2,127 @@
 #
 # Nagios systemd service status check
 #
-# This is a Nagios script which checks if the number of services
-# with a given status is exceeded. A list of services to check
-# can be provided.
+# This is a Nagios script to check all services know to "systemd".
+# It checks for anomalies of those services like the ones not anymore
+# defined but still running, and them being dead.  Normally returns
+# at most warning, if no critical services are specified.  Returns
+#
+# * critical when a critical service is failed
+# * warning when a critical service is not running
+# * warning when a non-critical service is failed
+# * warning for other anomalies.
 #
 # Copyright (c) 2016, InnoGames GmbH
 #
 
-import os
-import re
+import subprocess
 import sys
 
 from argparse import ArgumentParser
 
 
 command = 'systemctl --all --no-legend --no-pager list-units'
-default_critical_services = [nginx]
 
 
-class Problem: 
+class Problem:
+    """Enum for problems that can apply to the services"""
+
     # From more important to less
     failed = 0
     dead = 1
     not_loaded_but_not_inactive = 2
     not_loaded_but_not_dead = 3
 
-    
+
 def parse_args():
+    """Parse the arguments
+
+    We are returning them as a dict for callers convenience.
+    """
     parser = ArgumentParser()
     parser.add_argument(
-        '--critical-services',
-        nargs='*',
-        help='list of critical services to check',
+        '-s',
+        action='append',
+        dest='critical_services',
+        default=[],
+        help='service to return critical when failed',
     )
 
     return vars(parser.parse_args())
 
-    
+
 def main(critical_services):
-    if not critical_services:
-        critical_services = default_critical_services
+    """The main program"""
 
-    results = []
-    for line in os.popen(command).readlines():
+    try:
+        output = subprocess.check_output(command.split())
+    except subprocess.CalledProcessError as error:
+        print('UNKNOWN: ' + str(error))
+        sys.exit(3)
+
+    criticals = []
+    warnings = []
+    for line in output.splitlines():
         service_split = line.strip().split(None, 4)
+        service_name = service_split[0]
+        problem = check_service(*service_split[1:4])
 
-        result = check_service(*service_split[1:4])
-        if result:
-            results.append((result, service_split[0]))
-     
-    if any(r == Problem.failed and s in critical_services for r, s in results):
-        print('CRITICAL: ' + get_message(results))
+        if problem:
+            if service_name in critical_services:
+                if problem == Problem.failed:
+                    criticals.append((problem, service_name))
+                else:
+                    warnings.append((problem, service_name))
+            elif problem != Problem.dead:
+                warnings.append((problem, service_name))
+
+    criticals.sort()
+    warnings.sort()
+
+    if criticals:
+        print('CRITICAL: ' + get_message(criticals + warnings))
         sys.exit(2)
-    
-    if results:
-        print('WARNING: ' + get_message(results))
+    elif warnings:
+        print('WARNING: ' + get_message(warnings))
         sys.exit(1)
-    
-    print('OK')
-    sys.exit(0)
-    
-    
+    else:
+        print('OK')
+        sys.exit(0)
+
+
 def check_service(serv_load, serv_active, serv_sub):
+    """Detect problems of a service"""
     if serv_load == 'loaded':
         if serv_active == 'failed' or serv_sub == 'failed':
             return Problem.failed
-        
+
         if serv_sub == 'dead':
             return Problem.dead
     else:
         if serv_active != 'inactive':
             return Problem.not_loaded_but_not_inactive
-            
+
         if serv_sub != 'dead':
             return Problem.not_loaded_but_not_dead
-                 
- 
-def get_message(results):
-    problem_names = {v, k for k, v in vars(Problem) if isinstance(v, int)}
+
+
+def get_message(problems):
+    """Format the message to print out"""
+    problem_names = {
+        v: k.replace('_', ' ')
+        for k, v in vars(Problem).items()
+        if isinstance(v, int)
+    }
     message = ''
     last_problem = None
-    for problem, service in results.sorted():
+    for problem, service in problems:
         if problem != last_problem:
-            message += problem_names[problem].replace('_', ' ') + ': '
+            message += problem_names[problem] + ': '
             last_problem = problem
         message += service + ' '
-    
+
     return message
-  
-  
+
+
 if __name__ == '__main__':
     main(**parse_args())
