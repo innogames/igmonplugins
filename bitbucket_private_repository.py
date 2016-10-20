5d0319afc56621c0960e24f7fc2d6275f691dc45
Frederik Bauer <frederik.bauer@innogames.com>
2016-10-20 11:14:06 +0200

    [CLEANUP]

--- bitbucket_private_repository.py
+++ bitbucket_private_repository.py
@@ -50,32 +50,31 @@ from requests.auth import HTTPBasicAuth
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('--base-url', dest='base_url',
+    parser.add_argument('--base-url',
                         help='the base url of the application you want to '
                              'check (e.g. https://sub.example.com)')
-    parser.add_argument('--auth', dest='auth', choices=['basic', 'oauth'],
-                        help=('authentication mode to use. basic uses '
-                              'username and password. oauth uses private key '
-                              '(with/out passphrase) and consumer key. '
-                              'without --auth script will try an anonymous '
-                              'access.')
+    parser.add_argument('--auth', choices=('basic', 'oauth'),
+                        help='authentication mode to use. basic uses '
+                             'username and password. oauth uses private key '
+                             '(with/out passphrase) and consumer key. '
+                             'without --auth script will try an anonymous '
+                             'access.'
                         )
-    parser.add_argument('--username', dest='username',
+    parser.add_argument('--username',
                         help='the username for basic authentication')
-    parser.add_argument('--password', dest='password',
+    parser.add_argument('--password',
                         help='the password for basic authentication')
-    parser.add_argument('--consumer-key', dest='consumer_key',
+    parser.add_argument('--consumer-key',
                         help="consumer key for oauth authentication")
-    parser.add_argument('--consumer_secret', dest='consumer_secret',
+    parser.add_argument('--consumer_secret',
                         help="consumer secret for oauth authentication")
-    parser.add_argument('--private-key', dest='private_key',
-                        help="private key for oauth")
-    parser.add_argument('--passphrase', dest='passphrase',
+    parser.add_argument('--private-key', help="private key for oauth")
+    parser.add_argument('--passphrase',
                         help="possible passphrase for the private key")
-    parser.add_argument('--format', dest='format',
+    parser.add_argument('--format',
                         help=("the format of the final print out. for the "
                               "format the repository object will be passed"))
-    parser.add_argument('--delete', dest='delete', action="store_true",
+    parser.add_argument('--delete', action="store_true",
                         help="if found repositories should be deleted")
     return parser.parse_args()
 
@@ -86,16 +85,10 @@ def main(args):
     auth = parse_auth_argument(args)
 
     repositories = fetch_repositories(base_url, auth)
-    private_repos = []
-    for repository in repositories:
-        if 'origin' in repository:
-            continue
-
-        if not repository['project']['type'] == 'PERSONAL':
-            continue
-
-        private_repos.append(repository)
-        # print(json.dumps(repository))
+    private_repos = [
+        repo for repo in repositories
+        if 'origin' not in repo and repo['project']['type'] != 'PERSONAL'
+        ]
 
     if not private_repos:
         # no private repos were found
@@ -107,10 +100,9 @@ def main(args):
     else:
         format_string = ('{repo[project][owner][name]}: {repo[name]} '
                          '({repo[links][self][0][href]})\n')
-    string = ''
-    for repository in private_repos:
-        string += format_string.format(repo=repository)
     # private repos were found
+    string = ''.join(format_string.format(repo=r)
+                     for r in private_repos)
     print('WARNING: {amount} private repositories found:\n{0}'
           .format(string, amount=len(private_repos)))
     exit(2)
@@ -120,8 +112,8 @@ def parse_auth_argument(args):
     auth = args.auth
     if auth == 'basic':
         if not (args.username and args.password):
-            print(('For basic authentication, \'username\' and \'password\' '
-                   'parameter are needed'))
+            print("For basic authentication, 'username' and 'password' "
+                  "parameter are needed")
             exit(3)
         auth = HTTPBasicAuth(args.username, args.password)
     elif auth == 'oauth':
@@ -165,12 +157,12 @@ def get_oauth1session(consumer_key, consumer_secret, private_key,
                       passphrase):
     from Crypto.PublicKey import RSA
     from requests_oauthlib import OAuth1
-    f = open(private_key, 'r')
-    rsa_key = RSA.importKey(f.read(), passphrase)
+    with open(private_key, 'r') as fd:
+        rsa_key = RSA.importKey(fd.read(), passphrase)
 
     return OAuth1(client_key=consumer_key, client_secret=consumer_secret,
                   signature_method='RSA-SHA1', rsa_key=rsa_key)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(parse_args())
