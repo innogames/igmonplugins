5461a1a8d069b235e9a9036a7f0983ee3d2a7223
Frederik Bauer <frederik.bauer@innogames.com>
2016-11-09 15:31:19 +0100

    Refactor `check_multiple.py` against PEP8

--- src/check_multiple.py
+++ src/check_multiple.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 
-import argparse
 import subprocess
 import sys
+from argparse import ArgumentParser
 
-parser = argparse.ArgumentParser(description='Executes origin_command as many times with an element of iterate_over as iterate_over passed')
-parser.add_argument('origin_command', help='Original command with base params. e.g. "check_rabbitmq_queue --vhost=events -H localhost --port=15672 -u user -p pass --warning=50 --critical=100 --queue"')
-parser.add_argument('iterate_over', nargs='+', help='Queue list separated by space. e.g. "queue1 queue2"')
-args=parser.parse_args()
+parser = ArgumentParser(
+    description='Executes origin_command as many times with an element of '
+                'iterate_over as iterate_over passed')
+parser.add_argument(
+    'origin_command',
+    help='Original command with base params. e.g. '
+         '"check_rabbitmq_queue --vhost=events -H localhost --port=15672 '
+         '-u user -p pass --warning=50 --critical=100 --queue"'
+)
+parser.add_argument('iterate_over', nargs='+',
+                    help='Queue list separated by space. e.g. "queue1 queue2"')
+args = parser.parse_args()
 
 exit_code = 0
 message = ''
 subs = {}
 
 for iterate_param in args.iterate_over:
-    subs[iterate_param] = subprocess.Popen(args.origin_command + ' ' + iterate_param , stdout=subprocess.PIPE, shell=True)
+    subs[iterate_param] = subprocess.Popen(
+        args.origin_command + ' ' + iterate_param,
+        stdout=subprocess.PIPE,
+        shell=True)
 
 for iterate_param, process in subs.iteritems():
     out, err = process.communicate()
@@ -23,8 +34,8 @@ for iterate_param, process in subs.iteritems():
         message += '(' + iterate_param + '): ' + out + '\n'
 
 if not message:
-    print 'Everything is fine'
+    print('Everything is fine')
 else:
-    print 'Found some problems: ' + message.replace('\n', ' <> ')
+    print('Found some problems: ' + message.replace('\n', ' <> '))
 
 sys.exit(exit_code)
