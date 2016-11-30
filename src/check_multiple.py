6bded6535a319ef24ff4a8922f3ff09a55539ed7
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-11-30 16:50:09 +0100

    Fixed help message for check_multiple.py

--- src/check_multiple.py
+++ src/check_multiple.py
@@ -5,8 +5,8 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser(description='Executes origin_command as many times with an element of iterate_over as iterate_over passed')
-parser.add_argument('origin_command', help='Queue list separated by space. e.g. "queue1 queue2"')
-parser.add_argument('iterate_over', nargs='+', help='Original command with base params. e.g. "check_rabbitmq_queue --vhost=events -H localhost --port=15672 -u user -p pass --warning=50 --critical=100 --queue"')
+parser.add_argument('origin_command', help='Original command with base params. e.g. "check_rabbitmq_queue --vhost=events -H localhost --port=15672 -u user -p pass --warning=50 --critical=100 --queue"')
+parser.add_argument('iterate_over', nargs='+', help='Queue list separated by space. e.g. "queue1 queue2"')
 args=parser.parse_args()
 
 exit_code = 0
