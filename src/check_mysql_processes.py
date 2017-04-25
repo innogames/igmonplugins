a3938f081d37de1d8db9e1a165a3e6ab47d585a6
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-04-25 10:24:02 +0200

    check_mysql_processes.py: Improve code style

--- src/check_mysql_processes.py
+++ src/check_mysql_processes.py
@@ -1,41 +1,94 @@
 #!/usr/bin/env python
-import sys
-import MySQLdb
-import argparse
+'''InnoGames Monitoring Plugins - check_mysql_process_list.py
+
+Copyright (c) 2017, InnoGames GmbH
+'''
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from argparse import ArgumentParser
+from sys import exit
 import re
 
-#TODO: add verbose input for timestamps (1H, 30S, ...)
-#----- add for select columns (Sleep)
+from MySQLdb import connect
 
-#exitcodes
+# TODO: Add verbose input for timestamps (1H, 30S, ...)
+# TODO: Add for select columns (Sleep)
+
+
 class ExitCodes:
     ok = 0
     warning = 1
     critical = 2
 
-#init parser
-parser = argparse.ArgumentParser(description='Parameters for checking mysql processlist')
-parser.add_argument("-w", "--warning", type=str,nargs="+", help="Number of occasions with number seconds before a warning is given -- default:'1 of 90'", default="1 of 90")
-parser.add_argument("-c", "--critical", type=str,nargs="+", help="Number of occasions with number seconds before situation is critical -- default:'1 of 120'", default="1 of 120")
-parser.add_argument("-u", "--user", type=str)
-parser.add_argument("-p", "--passw", type=str)
 
 def parse_args():
+    parser = ArgumentParser(
+        description='Parameters for checking MySQL process list'
+    )
+    parser.add_argument('-w', '--warning', nargs='+', default='1 of 90', help=(
+        'Number of occasions with number seconds before a warning is given '
+        '(default: %(default)s)'
+    ))
+    parser.add_argument(
+        '-c', '--critical', nargs='+', default='1 of 120', help=(
+            'Number of occasions with number seconds before situation is '
+            'critical (default: %(default)s)'
+        )
+    )
+    parser.add_argument('-u', '--user')
+    parser.add_argument('-p', '--passwd')
+
     return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    rows = get_processlist()
+
+    if args.critical and check(args.critical, rows):
+        exit(ExitCodes.critical)
+    if check(args.warning, rows):
+        exit(ExitCodes.warning)
+    exit(ExitCodes.ok)
+
 
 def get_processlist():
     args = parse_args()
-    query = "select TIME,COMMAND,STATE from information_schema.processlist order by TIME DESC"
-    try :
-        db = MySQLdb.connect(host="localhost", user=args.user, passwd=args.passw)
-        cur = db.cursor()
-        cur.execute(query)
-        return cur.fetchall()
+    query = (
+        'SELECT TIME, COMMAND, STATE '
+        'FROM information_schema.PROCESSLIST '
+        'ORDER BY TIME DESC'
+    )
+    try:
+        db = connect(host='localhost', user=args.user, passwd=args.passwd)
+        try:
+            cursor = db.cursor()
+            cursor.execute(query)
+            return cursor.fetchall()
+        finally:
+            cursor.close()
     finally:
         db.close()
 
-def check(args,rows):
-    reg_pattern = "^([0-9]*).*?([0-9]*)$"
+
+def check(args, rows):
+    reg_pattern = '^([0-9]*).*?([0-9]*)$'
     for condition in args:
         match_array = re.match(reg_pattern, condition)
         counts_needed = int(match_array.group(1))
@@ -43,27 +96,13 @@ def check(args,rows):
         for row in rows:
             time = int(row[0])
             if time >= int(match_array.group(2)):
-                count = count + 1
+                count += 1
                 if count >= counts_needed:
                     return True
             else:
                 break
     return False
 
-
-def handle_rows(rows):
-    args = parse_args()
-
-    if check(args.critical,rows):
-        sys.exit(ExitCodes.critical)
-    elif check(args.warning,rows):
-        sys.exit(ExitCodes.warning)
-    sys.exit(ExitCodes.ok)
-
-
-def main():
-    rows = get_processlist()
-    handle_rows(rows)
 
 if __name__ == '__main__':
     main()
