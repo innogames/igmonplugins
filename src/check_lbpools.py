183e611af1f3d526d537355cf2a7889cefb86acd
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2017-07-25 14:28:05 +0200

    Simplify LB Pool checking algorighm
    All LB Pools are now loaded from iglb.json. This is a new place with all
    settings in it and all scripts should use it in future.

--- src/check_lbpools.py
+++ src/check_lbpools.py
@@ -24,39 +24,63 @@
 #
 
 import imp
+import json
 import subprocess
 import re
 
 # Nagios return codes
-exit_ok      = 0
-exit_warn    = 1
-exit_crit    = 2
+exit_ok = 0
+exit_warn = 1
+exit_crit = 2
 exit_unknown = 3
 
-def pairwise(it):
-    it = iter(it)
-    while True:
-        yield next(it), next(it)
+
+def main():
+    if not master_status():
+        return
+    known_pools = load_pools()
+    testtool_status = load_testtool_status()
+    compare_pools(known_pools, testtool_status, True)
 
 
 def master_status():
-    carp_settings = imp.load_source('carp_settings', '/etc/iglb/carp_settings.py')
+    carp_settings = imp.load_source(
+        'carp_settings',
+        '/etc/iglb/carp_settings.py'
+    )
 
     ret = False
     for ifname in carp_settings.ifaces.keys():
         # Read interface configuration:
-        p = subprocess.Popen(['/sbin/ifconfig', ifname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(
+            ['/sbin/ifconfig', ifname],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         ifconfig, err = p.communicate()
 
         for line in ifconfig.split("\n"):
             # Find carp lines, the look like this:
-            #carp: MASTER vhid 133 advbase 1 advskew 50
-            ifconfig_match = re.match(".*carp: ([A-Z]+) vhid ([0-9]+) advbase.*", line)
+            # carp: MASTER vhid 133 advbase 1 advskew 50
+            ifconfig_match = re.match(
+                ".*carp: ([A-Z]+) vhid ([0-9]+) advbase.*",
+                line
+            )
             if ifconfig_match:
                 status = ifconfig_match.group(1)
                 if status == 'MASTER':
                     ret = True
     return ret
+
+
+def load_pools():
+    with open('/etc/iglb/iglb.json') as jsonfile:
+        return json.load(jsonfile)['lbpools']
+
+
+def pairwise(it):
+    it = iter(it)
+    while True:
+        yield next(it), next(it)
 
 
 def load_testtool_status():
@@ -77,73 +101,93 @@ def load_testtool_status():
     return ret
 
 
-def load_known_pools():
-    poolnames = imp.load_source('poolnames', '/etc/iglb/poolnames.py')
-    return poolnames.poolnames
-
-
 def compare_pools(pools, testtool, send):
     output = ''
+
     if send:
         separator = "\27"
     else:
         separator = "\n"
+
     for pool_k, pool_v in pools.items():
-        in_testtool = False
-        if pool_k in testtool:
-            num_nodes = testtool[pool_k]['nodes_alive']
-            in_testtool = True
+        # Skip NAT-only rules
+        if not pool_v['protocol_port']:
+            continue
 
-        optimal_nodes = pool_v.get('optimal_nodes')
-        min_nodes = pool_v.get('min_nodes')
-        max_nodes = pool_v.get('max_nodes')
+        for proto in ['4', '6']:
+            if not pool_v.get('ip' + proto):
+                continue
 
-        if pool_v['has_healthchecks']:
-            if in_testtool:
-                if num_nodes == 0:
-                    exit_code = exit_crit
-                elif max_nodes is not None and (num_nodes > max_nodes):
-                    exit_code = exit_warn
-                elif num_nodes == optimal_nodes:
-                    exit_code = exit_ok
-                elif min_nodes is not None and (num_nodes >= min_nodes):
-                    exit_code = exit_ok
+            if proto == '6':
+                nagios_service = 'check_lbpool_6'
+            else:
+                nagios_service = 'check_lbpool'
+
+            in_testtool = False
+            testtool_name = pool_v['pf_name'] + '_' + proto
+            if testtool_name in testtool:
+                num_nodes = testtool[testtool_name]['nodes_alive']
+                in_testtool = True
+
+            optimal_nodes = len(pool_v['nodes'])
+            min_nodes = pool_v.get('min_nodes')
+            max_nodes = pool_v.get('max_nodes')
+
+            if pool_v['healthchecks']:
+                if in_testtool:
+                    if num_nodes == 0:
+                        exit_code = exit_crit
+                    elif max_nodes is not None and (num_nodes > max_nodes):
+                        exit_code = exit_warn
+                    elif num_nodes == optimal_nodes:
+                        exit_code = exit_ok
+                    elif min_nodes is not None and (num_nodes >= min_nodes):
+                        exit_code = exit_ok
+                    else:
+                        exit_code = exit_warn
+                    output += (
+                        '{}\t{}\t{}\t{} of '
+                        '(min: {}, max: {}, all: {}) nodes alive{}'
+                    ).format(
+                        pool_k,
+                        nagios_service,
+                        exit_code,
+                        num_nodes,
+                        min_nodes,
+                        max_nodes,
+                        optimal_nodes,
+                        separator,
+                        )
+                    separator
                 else:
-                    exit_code = exit_warn
-                output += "{}\t{}\t{}\t{}(min: {}, max: {})/{} nodes alive{}".format(
-                    pool_v['nagios_host'],
-                    pool_v['nagios_service'],
-                    exit_code,
-                    num_nodes,
-                    min_nodes,
-                    max_nodes,
-                    optimal_nodes,
-                    separator,
-                    )
-                separator
+                    output += '{}\t{}\t{}\tNot monitored by testtool{}'.format(
+                        pool_k,
+                        nagios_service,
+                        exit_unknown,
+                        separator
+                        )
             else:
-                output += "{}\t{}\t{}\tNot monitored by testtool{}".format(
-                    pool_v['nagios_host'],
-                    pool_v['nagios_service'],
-                    exit_unknown,
-                    separator
-                    )
-        else:
-            if optimal_nodes == 1:
-                output += "{}\t{}\t{}\tHas no healthchecks and only 1 node{}".format(
-                    pool_v['nagios_host'],
-                    pool_v['nagios_service'],
-                    exit_ok,
-                    separator
-                    )
-            else:
-                output += "{}\t{}\t{}\tHas no healthchecks but {} nodes{}".format(
-                    pool_v['nagios_host'],
-                    pool_v['nagios_service'],
-                    exit_warn,
-                    optimal_nodes,
-                    separator
-                    )
+                if optimal_nodes == 1:
+                    output += (
+                        '{}\t{}\t{}\t'
+                        'Has no healthchecks and only 1 node{}'
+                    ).format(
+                        pool_k,
+                        nagios_service,
+                        exit_ok,
+                        separator
+                        )
+                else:
+                    output += (
+                        '{}\t{}\t{}\t'
+                        'Has no healthchecks but {} nodes{}'
+                    ).format(
+                        pool_k,
+                        nagios_service,
+                        exit_warn,
+                        optimal_nodes,
+                        separator
+                        )
     if send:
         for monitor in ('af', 'aw'):
             nsca = subprocess.Popen(
@@ -154,19 +198,11 @@ def compare_pools(pools, testtool, send):
                     '-c',
                     '/usr/local/etc/nagios/send_nsca.cfg',
                 ],
-                stdin = subprocess.PIPE,
+                stdin=subprocess.PIPE,
             )
             nsca.communicate(output)
     else:
-        print output
-
-
-def main():
-    if not master_status():
-        return
-    known_pools = load_known_pools()
-    testtool_status = load_testtool_status()
-    result = compare_pools(known_pools, testtool_status, True)
+        print(output)
 
 
 if __name__ == "__main__":
