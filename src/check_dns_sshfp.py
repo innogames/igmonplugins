c74efe537dcfe36eddd3d3438411dd6ff06b99d7
Emre Hasegeli <emre.hasegeli@innogames.com>
2018-06-13 12:47:28 +0200

    check_dns_sshfp.py: Don't use DNS search path

--- src/check_dns_sshfp.py
+++ src/check_dns_sshfp.py
@@ -35,7 +35,10 @@ from dns.resolver import query as dns_query
 
 
 def main():
-    fqdn = getfqdn()
+
+    # If we don't append the trailing dot, the query would try the DNS search
+    # path.
+    fqdn = getfqdn() + '.'
     result = dns_query(fqdn, SSHFP, raise_on_no_answer=False)
 
     if not result.rrset:
