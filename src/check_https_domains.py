5185660aeb9fe48670c98524fcbcd65dcb5f4f1b
Luiz Amaral <luiz.amaral@innogames.com>
2019-07-10 14:37:54 +0200

    Enhance the help messages

--- src/check_https_domains.py
+++ src/check_https_domains.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """InnoGames Monitoring Plugins - HTTPS Domains Check
 
-Copyright (c) 2017 InnoGames GmbH
+Copyright (c) 2019 InnoGames GmbH
 """
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@ import ssl
 import sys
 from argparse import ArgumentParser, RawTextHelpFormatter
 from datetime import datetime, timedelta
-from dateutil.parser import parse
+from dateutil.parser import parse as parse_date
 from dateutil.tz import tzutc
 from OpenSSL import crypto
 
@@ -33,26 +33,54 @@ from OpenSSL import crypto
 warn = 30
 crit = 2
 
+
 def parse_args():
     parser = ArgumentParser(
-        description='Check domains',
+        description=(
+            'This Nagios check retrieves certificates for the domains '
+            'specified in the -d attribute and check for their expiration '
+            'dates.\nThe check goes into critical if less than {crit} days '
+            'are remaining and goes into warning if less than {warn} days '
+            'are remaining.'
+            .format(crit=crit, warn=warn)
+        ),
         formatter_class=RawTextHelpFormatter
     )
 
     parser.add_argument(
         '-s', dest='hostname', required=True,
-        help='hostname'
+        help='Hostname of the host in Nagios. Only used for output building.'
     )
     parser.add_argument(
         '-i', dest='ip', required=True,
-        help='ip of host'
+        help='IP of the host. The address where the certificate will be '
+             'retrieved from.'
     )
     parser.add_argument(
         '-d', dest='domains', required=True,
-        help='domains of host'
+        help='Domains to retrieve certificates for. For multiple domains, '
+             'provide them as single string, comma separated.'
     )
 
     return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    domains = get_domains(args.domains)
+
+    if not domains or domains == ['$_HOSTDOMAINS$']:
+        message = (
+            'UNKNOWN - No domain found for host: {}, ip: {}'
+            .format(args.hostname, args.ip)
+        )
+        print(message)
+        sys.exit(3)
+
+    state, output = get_check_result(domains, args.ip)
+    print(output)
+    sys.exit(state)
+
 
 def get_domains(domains):
     domains = domains.split(',')
@@ -71,11 +99,13 @@ def fetch_cert_info(domain, ip):
             ssl.DER_cert_to_PEM_cert(sock.getpeercert(True))
         )
     common_name = cert.get_subject().commonName
-    not_after = parse(cert.get_notAfter().decode('utf-8'))
+    not_after = parse_date(cert.get_notAfter().decode('utf-8'))
     remaining = not_after - datetime.now(tzutc())
 
-    data = {'remaining': remaining, 'common_name': common_name,
-            'domain': domain, 'not_after': not_after}
+    data = {
+        'remaining': remaining, 'common_name': common_name,
+        'domain': domain, 'not_after': not_after
+    }
     return data
 
 
@@ -87,7 +117,7 @@ def get_check_result(domains, ip):
         expirations.append(fetch_cert_info(domain, ip))
 
     if not expirations:
-        return_result(3, 'Could not obtain expiration dates')
+        return (3, 'Could not obtain expiration dates')
 
     # Certificates that are closer to the expiration date are shown first
     expirations.sort(key=lambda x: x['remaining'])
@@ -96,34 +126,31 @@ def get_check_result(domains, ip):
         state = 2
         if expirations[0]['remaining'] < timedelta(0):
             output.append(
-                'CRITICAL - There are certificates expired for {} days'.format(
-                    -expirations[0]['remaining'].days
-                )
+                'CRITICAL - There are certificates expired for {} days'
+                .format(-expirations[0]['remaining'].days)
             )
         else:
             output.append(
-                'CRITICAL - There are certificates expiring in {} days'.format(
-                    expirations[0]['remaining'].days
-                )
+                'CRITICAL - There are certificates expiring in {} days'
+                .format(expirations[0]['remaining'].days)
             )
     elif expirations[0]['remaining'] <= timedelta(days=warn):
         state = 1
         output.append(
-            'WARNING - There are certificates expiring in {} days'.format(
-                expirations[0]['remaining'].days
-            )
+            'WARNING - There are certificates expiring in {} days'
+            .format(expirations[0]['remaining'].days)
         )
     else:
         state = 0
         output.append(
-            'OK - Next certificate expiration is in {} days'.format(
-                expirations[0]['remaining'].days
-            )
+            'OK - Next certificate expiration is in {} days'
+            .format(expirations[0]['remaining'].days)
         )
 
     for expiration in expirations:
         output.append(
-            'Certificate {} for domain {} will expire in {} days ({})'.format(
+            'Certificate {} for domain {} will expire in {} days ({})'
+            .format(
                 expiration['common_name'], expiration['domain'],
                 expiration['remaining'].days,
                 expiration['not_after'].strftime("%Y-%m-%d")
@@ -131,25 +158,6 @@ def get_check_result(domains, ip):
         )
 
     return (state, '\n'.join(output))
-
-
-def return_result(state, message):
-    print(message)
-    sys.exit(state)
-
-
-def main():
-    args = parse_args()
-    domains = get_domains(args.domains)
-
-    if not domains or domains == ['$_HOSTDOMAINS$']:
-        return_result(
-            3, 'UNKNOWN - No domain found for host: {}, ip: {}'.format(
-                args.hostname, args.ip
-            )
-        )
-    state, output = get_check_result(domains, args.ip)
-    return_result(state, output)
 
 
 if __name__ == '__main__':
