3c177f8d7c9ae5ef8bfddbe32b8a2fbfed781196
Matthias Klein <matthias.klein@innogames.com>
2019-10-22 11:13:20 +0200

    Allow Auth and SSL for Elastic checks

--- src/check_es_cluster_state.py
+++ src/check_es_cluster_state.py
@@ -25,6 +25,7 @@ Copyright (c) 2019 InnoGames GmbH
 import sys
 from argparse import ArgumentParser, RawTextHelpFormatter
 from elasticsearch import Elasticsearch
+from ssl import create_default_context
 
 def parse_args():
     parser = ArgumentParser(
@@ -37,19 +38,41 @@ def parse_args():
     parser.add_argument(
         'host', help='Elasticsearch host to run the query against'
     )
+    parser.add_argument(
+        '-u', '--user', help='Monitoring user'
+    )
+    parser.add_argument(
+        '-p', '--password', help='Password for monitoring user'
+    )
+    parser.add_argument(
+        '--root_ca', help='CA file matching the servers certificate'
+    )
 
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    es = Elasticsearch(args.host)
+
+    connect_params = {}
+    if args.root_ca:
+        connect_params['scheme'] = 'https'
+        connect_params['ssl_context'] = create_default_context(cafile = args.root_ca)
+
+    if args.user and args.password:
+        connect_params['http_auth'] = (args.user, args.password)
+
+    es = Elasticsearch([args.host], **connect_params)
+
     health = es.cluster.health()
+
     if health['status'] == 'red':
         print('Cluster status is red')
         sys.exit(2)
+
     if health['status'] == 'yellow':
         print('Cluster status is yellow')
         sys.exit(1)
+
     print('Cluster status is green')
 
 if __name__ == '__main__':
