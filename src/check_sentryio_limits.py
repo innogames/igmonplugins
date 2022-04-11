51166ed52e1e40e400c2357e095d220e932af39d
Raimund S <raimund-s@users.noreply.github.com>
2022-04-11 16:14:27 +0200

    Update src/check_sentryio_limits.py
    Co-authored-by: Daniel Kröger <kofrezo@users.noreply.github.com>

--- src/check_sentryio_limits.py
+++ src/check_sentryio_limits.py
@@ -25,7 +25,7 @@ Copyright (c) 2022 InnoGames GmbH
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from typing import List
 import requests
 import sys
