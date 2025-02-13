f926c2bce7ca2dcffb2ed3b75e7e364507314710
Gürkan Gür <gurkan.gur@innogames.com>
2025-02-13 14:17:07 +0100

    mysql: Check if the secondary is catching up

--- src/check_mysql_slave_delay.py
+++ src/check_mysql_slave_delay.py
@@ -22,8 +22,13 @@ Copyright (c) 2020 InnoGames GmbH
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from mysql.connector import connect
+import json
 import optparse
+import time
+
+from mysql.connector import connect
+
+STATE_FILE = '/tmp/mysql_replication_state.json'
 
 ERR = {'CRITICAL': 2, 'WARNING': 1, 'OK': 0}
 
@@ -31,31 +36,40 @@ ERR = {'CRITICAL': 2, 'WARNING': 1, 'OK': 0}
 def parse_args():
     parser = optparse.OptionParser()
     parser.add_option(
-        '-w', '--warning',
+        '-w',
+        '--warning',
         help='Warning limit of seconds behind master',
-        dest='WARN_SEC_BEHIND_MASTER', action='store',
-        type='int', default=60
+        dest='WARN_SEC_BEHIND_MASTER',
+        action='store',
+        type='int',
+        default=60,
     )
     parser.add_option(
-        '-c', '--critical',
+        '-c',
+        '--critical',
         help='Critical limit of seconds behind master',
-        dest='CRIT_SEC_BEHIND_MASTER', action='store',
-        type='int', default=120
+        dest='CRIT_SEC_BEHIND_MASTER',
+        action='store',
+        type='int',
+        default=120,
     )
     parser.add_option(
-        '-n', '--name',
-        help='Name of slave to check (for multi-source '
-             'replication)',
-        action='store'
+        '-n',
+        '--name',
+        help='Name of slave to check (for multi-source replication)',
+        action='store',
     )
     parser.add_option(
-        '-u', '--user', help='Name of user for mysql connection',
-        action='store'
+        '-u',
+        '--user',
+        help='Name of user for mysql connection',
+        action='store',
     )
     parser.add_option(
-        '-p', '--password',
+        '-p',
+        '--password',
         help='Password of user for mysql connection',
-        action='store'
+        action='store',
     )
     parser.add_option('--unix-socket', default='/var/run/mysqld/mysqld.sock')
     (opts, args) = parser.parse_args()
@@ -64,19 +78,61 @@ def parse_args():
 
 def get_server_status(opts):
     if opts.user:
-        db = connect(user=opts.user, passwd=opts.password,
-                     unix_socket=opts.unix_socket)
+        db = connect(
+            user=opts.user, passwd=opts.password, unix_socket=opts.unix_socket
+        )
     else:
         db = connect(unix_socket=opts.unix_socket)
     cur = db.cursor()
     if opts.name:
-        cur.execute("SHOW SLAVE STATUS FOR CHANNEL '{}'".format(opts.name))
+        cur.execute(f"SHOW SLAVE STATUS FOR CHANNEL '{opts.name}'")
     else:
-        cur.execute("SHOW SLAVE STATUS")
+        cur.execute('SHOW SLAVE STATUS')
     res = [dict(zip(cur.column_names, r)) for r in cur.fetchall()]
     cur.close()
     db.close()
     return res[0]
+
+
+def load_previous_state() -> tuple[int, float]:
+    try:
+        with open(STATE_FILE) as f:
+            state = json.load(f)
+            return state['seconds_behind'], state['check_time']
+    except (FileNotFoundError, json.JSONDecodeError, KeyError):
+        # If file does not exist or is corrupted, return 0
+        # so we can disregard the previous state
+        return 0, 0
+
+
+def save_current_state(seconds_behind: int) -> None:
+    state = {'seconds_behind': seconds_behind, 'check_time': time.time()}
+    try:
+        with open(STATE_FILE, 'w') as f:
+            json.dump(state, f)
+    except Exception:
+        pass  # Fail silently as this is just a helper functionality
+
+
+def is_catching_up(
+    current_seconds: int, previous_seconds: int, previous_time: float
+) -> bool:
+    """Check if the slave is actually catching up with the master"""
+
+    if previous_time == 0:
+        return False
+
+    time_diff = time.time() - previous_time
+    if time_diff == 0:
+        return False
+
+    delay_diff = current_seconds - previous_seconds
+
+    # Calculate how fast it is catching up
+    catch_up_rate = delay_diff / time_diff
+
+    # Negative means catching up
+    return catch_up_rate < 0
 
 
 def check_server(opts):
@@ -84,14 +140,38 @@ def check_server(opts):
         s = get_server_status(opts)
     except Exception as e:
         return ['CRITICAL', str(e.args)]
-    msg = "SLAVE IO Running: " + s['Slave_IO_Running']
-    msg += ", SLAVE SQL Running: " + s['Slave_SQL_Running']
-    msg += ", " + str(s['Seconds_Behind_Master']) + " secs behind Master"
-    if s['Slave_IO_Running'] != "Yes" or s['Slave_SQL_Running'] != "Yes" \
-            or s['Seconds_Behind_Master'] > opts.CRIT_SEC_BEHIND_MASTER:
+
+    current_behind = s['Seconds_Behind_Master']
+    prev_behind, prev_time = load_previous_state()
+
+    msg = 'SLAVE IO Running: ' + s['Slave_IO_Running']
+    msg += ', SLAVE SQL Running: ' + s['Slave_SQL_Running']
+    msg += ', ' + str(current_behind) + ' secs behind Master'
+
+    # Save current state for next check
+    save_current_state(current_behind)
+
+    if s['Slave_IO_Running'] != 'Yes' or s['Slave_SQL_Running'] != 'Yes':
         return ['CRITICAL', msg]
-    if s['Seconds_Behind_Master'] > opts.WARN_SEC_BEHIND_MASTER:
+
+    # Critical threshold check with catch-up logic
+    if current_behind > opts.CRIT_SEC_BEHIND_MASTER:
+        if is_catching_up(current_behind, prev_behind, prev_time):
+            # If catching up, use doubled threshold
+            if current_behind > (opts.CRIT_SEC_BEHIND_MASTER * 2):
+                return ['CRITICAL', msg + ' (catching up, but extremely high)']
+            return ['WARNING', msg + ' (catching up from critical)']
+        return ['CRITICAL', msg]
+
+    # Warning threshold check with catch-up logic
+    if current_behind > opts.WARN_SEC_BEHIND_MASTER:
+        if is_catching_up(current_behind, prev_behind, prev_time):
+            # If catching up, use doubled threshold
+            if current_behind > (opts.WARN_SEC_BEHIND_MASTER * 2):
+                return ['WARNING', msg + ' (catching up, but still high)']
+            return ['OK', msg + ' (catching up)']
         return ['WARNING', msg]
+
     # There could be gaps in replication. We check here, because during delays
     # this might happen. To spot a gap, we check if there is more than one
     # colon per GTID line
@@ -99,13 +179,14 @@ def check_server(opts):
         if line.count(':') > 1:
             msg = 'Gaps in replication detected: ' + line
             return ['CRITICAL', msg]
+
     return ['OK', msg]
 
 
 def main():
-    opts, args = parse_args()
+    opts, _ = parse_args()
     status = check_server(opts)
-    print(": ".join(status))
+    print(': '.join(status))
     exit(ERR[status[0]])
 
 
