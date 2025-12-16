ae5eeaf3d5125ae75ba6c078374e385d68dd8c88
Gürkan <307899+seqizz@users.noreply.github.com>
2025-12-16 13:31:36 +0100

    check_https_domains: Do proper strip on domain names (#360)

--- src/check_https_domains.py
+++ src/check_https_domains.py
@@ -26,6 +26,7 @@ import ssl
 import sys
 from argparse import ArgumentParser, RawTextHelpFormatter
 from datetime import datetime, timedelta
+
 from dateutil.parser import parse as parse_date
 from dateutil.tz import tzutc
 from OpenSSL import crypto
@@ -40,35 +41,46 @@ def parse_args():
         description=(
             'This Nagios check retrieves certificates for the domains '
             'specified in the -d attribute and check for their expiration '
-            'dates.\nThe check goes into critical if less than {crit} days '
-            'are remaining and goes into warning if less than {warn} days '
+            f'dates.\nThe check goes into critical if less than {crit} days '
+            f'are remaining and goes into warning if less than {warn} days '
             'are remaining.'
-            .format(crit=crit, warn=warn)
         ),
-        formatter_class=RawTextHelpFormatter
+        formatter_class=RawTextHelpFormatter,
     )
 
     parser.add_argument(
-        '-s', dest='hostname', required=True,
-        help='Hostname of the host in Nagios. Only used for output building.'
+        '-s',
+        dest='hostname',
+        required=True,
+        help='Hostname of the host in Nagios. Only used for output building.',
     )
     parser.add_argument(
-        '-i', dest='ip', required=True,
+        '-i',
+        dest='ip',
+        required=True,
         help='IP of the host. The address where the certificate will be '
-             'retrieved from.'
+        'retrieved from.',
     )
     parser.add_argument(
-        '-p', '--port', type=int, default=443,
-        help='Port on the host where Nagios will connect to. Defaults to 443.'
+        '-p',
+        '--port',
+        type=int,
+        default=443,
+        help='Port on the host where Nagios will connect to. Defaults to 443.',
     )
     parser.add_argument(
-        '-t', '--timeout', type=int, default=2,
-        help='Socket timeout (in seconds) for each SSL connection'
+        '-t',
+        '--timeout',
+        type=int,
+        default=2,
+        help='Socket timeout (in seconds) for each SSL connection',
     )
     parser.add_argument(
-        '-d', dest='domains', required=True,
+        '-d',
+        dest='domains',
+        required=True,
         help='Domains to retrieve certificates for. For multiple domains, '
-             'provide them as single string, comma separated.'
+        'provide them as single string, comma separated.',
     )
 
     return parser.parse_args()
@@ -79,10 +91,7 @@ def main():
     domains = get_domains(args.domains)
 
     if not domains or domains == ['$_HOSTDOMAINS$']:
-        message = (
-            'UNKNOWN - No domain found for host: {}, ip: {}'
-            .format(args.hostname, args.ip)
-        )
+        message = f'UNKNOWN - No domain found for host: {args.hostname}, ip: {args.ip}'
         print(message)
         sys.exit(3)
 
@@ -99,7 +108,7 @@ def main():
 
 
 def get_domains(domains):
-    domains = domains.split(',')
+    domains = [d.strip() for d in domains.split(',')]
     if len(domains) == 1 and 'None' in domains:
         domains = []
     return domains
@@ -113,7 +122,7 @@ def fetch_cert_info(domain, ip, port, timeout):
     with context.wrap_socket(conn, server_hostname=domain) as sock:
         cert = crypto.load_certificate(
             crypto.FILETYPE_PEM,
-            ssl.DER_cert_to_PEM_cert(sock.getpeercert(True))
+            ssl.DER_cert_to_PEM_cert(sock.getpeercert(True)),
         )
 
     common_name = cert.get_subject().commonName
@@ -121,8 +130,10 @@ def fetch_cert_info(domain, ip, port, timeout):
     remaining = not_after - datetime.now(tzutc())
 
     data = {
-        'remaining': remaining, 'common_name': common_name,
-        'domain': domain, 'not_after': not_after
+        'remaining': remaining,
+        'common_name': common_name,
+        'domain': domain,
+        'not_after': not_after,
     }
     return data
 
@@ -136,7 +147,10 @@ def get_check_result(domains, ip, port, timeout):
             cert_info = fetch_cert_info(domain, ip, port, timeout)
             expirations.append(cert_info)
         except (socket.timeout, TimeoutError):
-            return (3, f'The connection to the destination host {domain} timed out')
+            return (
+                3,
+                f'The connection to the destination host {domain} timed out',
+            )
 
     if not expirations:
         return (3, 'Could not obtain expiration dates')
@@ -148,34 +162,38 @@ def get_check_result(domains, ip, port, timeout):
         state = 2
         if expirations[0]['remaining'] < timedelta(0):
             output.append(
-                'CRITICAL - There are certificates expired for {} days'
-                .format(-expirations[0]['remaining'].days)
+                'CRITICAL - There are certificates expired for {} days'.format(
+                    -expirations[0]['remaining'].days
+                )
             )
         else:
             output.append(
-                'CRITICAL - There are certificates expiring in {} days'
-                .format(expirations[0]['remaining'].days)
+                'CRITICAL - There are certificates expiring in {} days'.format(
+                    expirations[0]['remaining'].days
+                )
             )
     elif expirations[0]['remaining'] <= timedelta(days=warn):
         state = 1
         output.append(
-            'WARNING - There are certificates expiring in {} days'
-            .format(expirations[0]['remaining'].days)
+            'WARNING - There are certificates expiring in {} days'.format(
+                expirations[0]['remaining'].days
+            )
         )
     else:
         state = 0
         output.append(
-            'OK - Next certificate expiration is in {} days'
-            .format(expirations[0]['remaining'].days)
+            'OK - Next certificate expiration is in {} days'.format(
+                expirations[0]['remaining'].days
+            )
         )
 
     for expiration in expirations:
         output.append(
-            'Certificate {} for domain {} will expire in {} days ({})'
-            .format(
-                expiration['common_name'], expiration['domain'],
+            'Certificate {} for domain {} will expire in {} days ({})'.format(
+                expiration['common_name'],
+                expiration['domain'],
                 expiration['remaining'].days,
-                expiration['not_after'].strftime("%Y-%m-%d")
+                expiration['not_after'].strftime('%Y-%m-%d'),
             )
         )
 
